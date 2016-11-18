b70a95c5294805b7aec1851ae5819a3ea662c2cc
Sebastian Mohr <bashman2000@ymail.com>
2016-11-18 08:44:12 -0700

    Added notes to models v0.5

--- dynamics/admin.py
+++ dynamics/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from dynamics.models import Site, Price
+from dynamics.models import Site, Price, Note
 
 # Register your models here.
 admin.site.register(Site)
 admin.site.register(Price)
+admin.site.register(Note)
--- dynamics/models.py
+++ dynamics/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+# For priceing teirs
 class Price(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
@@ -10,16 +11,28 @@ class Price(models.Model):
     num_page_max = models.DecimalField(decimal_places=0, max_digits=2)
     num_page_base = models.DecimalField(decimal_places=0, max_digits=2)
     is_dynamic = models.BooleanField()
+    note = models.TextField()
 
     def __str__(self):
         return self.name
 
+# For sites we have made
 class Site(models.Model):
     name = models.CharField(max_length=255)
     cost = models.DecimalField(decimal_places=2, max_digits=6)
     price_teir = models.CharField(max_length=255)
     url = models.URLField(max_length=255)
     date_made = models.DateTimeField()
+    note = models.TextField()
+
+    def __str__(self):
+        return self.names
+
+# For our communication
+class Note(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    creater = models.CharField(max_length=125)
 
     def __str__(self):
         return self.names
